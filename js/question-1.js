a09a8eb15c411329c8387a2773f95900a60fa213
tnjensen <thomasnorupjensen@gmail.com>
2021-12-05 21:42:12 +0100

    Removed comments

--- js/question-1.js
+++ js/question-1.js
@@ -1,8 +1,5 @@
 /* Question 1 */
-/* function getRemainder(a,b){
-    return a % b;
-} */
-/* Converted to arrow function */
+/* Converting function to arrow function */
 const getRemainder = (a,b) => a % b;
 
 const result = console.log(getRemainder(5,3)); /* 2 */ 
--- js/question-2.js
+++ js/question-2.js
@@ -1,4 +1,3 @@
-/* API Key RAWG : 640bc89f838c450f998748b557361712 */
 
 const rawgResults = document.querySelector('.main-content');
 const apiKey = "640bc89f838c450f998748b557361712";
@@ -12,32 +11,34 @@ const games = [
         relaseDate: '2019-12-31'
     }
 ]
-/* const searchButton = document.querySelector('.search-btn'); */
 const error = document.querySelector('.error');
 const baseURL = 'https://api.rawg.io/api/games';
 
 
 async function getRawgData(){
-    error.innerHTML = "";
-    const url =  "?" + "dates=" + games[0].relaseDate + "," + games[1].relaseDate + "&ordering=rating" + "&key=" + apiKey;
-    /* console.log(baseURL + url); */
-    const response = await fetch(baseURL + url);
-    const records = await response.json();
-    /* console.log(records.results[0].ratings[0].title); */
-    rawgResults.innerHTML = "";
-    for(i = 0; i < records.results.length; i++){
-        if(i === 8){
-            break;
+    try{
+        error.innerHTML = "";
+        const url =  "?" + "dates=" + games[0].relaseDate + "," + games[1].relaseDate + "&ordering=rating" + "&key=" + apiKey;
+        const response = await fetch(baseURL + url);
+        const records = await response.json();
+        rawgResults.innerHTML = "";
+        for(i = 0; i < records.results.length; i++){
+            if(i === 8){
+                break;
+            }
+        
+            rawgResults.innerHTML += `<div>
+                                  <ul>
+                                  <li>Game ${i + 1}</li>
+                                    <li>name: ${records.results[i].name}</li>
+                                    <li>rating: ${records.results[0].ratings[0].title}</li>
+                                    <li>number of tags: ${records.results[i].tags.length}</li>
+                                  </ul>
+                                </div>`;
         }
-    
-        rawgResults.innerHTML += `<div>
-                              <ul>
-                              <li>Game ${i + 1}</li>
-                                <li>name: ${records.results[i].name}</li>
-                                <li>rating: ${records.results[0].ratings[0].title}</li>
-                                <li>number of tags: ${records.results[i].tags.length}</li>
-                              </ul>
-                            </div>`;
+    }
+    catch(error){
+        rawgResults.innerHTML = displayError("An error ocurred, please try again.");
     }
 }
 
