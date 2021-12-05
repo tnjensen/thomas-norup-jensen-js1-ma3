c8cdd1d564d318b04cc8c63608da8f2459bfa7b1
tnjensen <thomasnorupjensen@gmail.com>
2021-12-05 21:19:53 +0100

    Edited css

--- js/question-2.js
+++ js/question-2.js
@@ -12,7 +12,7 @@ const games = [
         relaseDate: '2019-12-31'
     }
 ]
-const searchButton = document.querySelector('.search-btn');
+/* const searchButton = document.querySelector('.search-btn'); */
 const error = document.querySelector('.error');
 const baseURL = 'https://api.rawg.io/api/games';
 
@@ -20,29 +20,26 @@ const baseURL = 'https://api.rawg.io/api/games';
 async function getRawgData(){
     error.innerHTML = "";
     const url =  "?" + "dates=" + games[0].relaseDate + "," + games[1].relaseDate + "&ordering=rating" + "&key=" + apiKey;
-    console.log(baseURL + url);
+    /* console.log(baseURL + url); */
     const response = await fetch(baseURL + url);
     const records = await response.json();
-    console.log(records.results[0].ratings[0].title);
-
-    /* if(results.length === 0){
-        error.innerHTML = "An error ocurred, please try again...";
-        return;
-    } */
+    /* console.log(records.results[0].ratings[0].title); */
+    rawgResults.innerHTML = "";
     for(i = 0; i < records.results.length; i++){
-        if(i === 9){
+        if(i === 8){
             return;
         }
     
         rawgResults.innerHTML += `<div>
                               <ul>
-                                <li>Name: ${records.results[i].name}</li>
-                                <li>Rating: ${records.results[0].ratings[0].title}</li>
-                                <li>Number of tags: ${records.results[i].tags.length}</li>
+                              <li>Game ${i + 1}</li>
+                                <li>name: ${records.results[i].name}</li>
+                                <li>rating: ${records.results[0].ratings[0].title}</li>
+                                <li>number of tags: ${records.results[i].tags.length}</li>
                               </ul>
                             </div>`;
     }
 }
 
-searchButton.onclick = getRawgData();
+getRawgData();
 
